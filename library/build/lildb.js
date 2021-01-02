830049e38ba61fb71b6a0389abe9def2bcb7b545
Mateo Morris <accounts@mateomorris.com>
2021-01-03 01:39:41 +0200

    clean up

--- library/build/lildb.js
+++ library/build/lildb.js
@@ -1,44 +1,25 @@
 let endpoint;
-async function runFbFunction(args) {
-  if (endpoint) {
-    return fetch("http://localhost:5000/lildb-8ff35/us-central1/db/foo").then((response) => response.json()).then((data) => {
-      console.log(data);
-      return data;
-    });
-  } else {
-    console.error("lildb needs an endpoint");
-    return {
-      data: null
-    };
-  }
-}
 export default {
   connect: (end) => {
     endpoint = end;
   },
   get: async (key) => {
-    const {data} = await runFbFunction({
-      type: "GET",
-      key
-    });
-    console.log(data);
-    return data;
+    return await fetch(`http://localhost:5000/lildb-8ff35/us-central1/db/${endpoint}?key=${key}`).then((response) => response.json());
   },
   set: async (key, value) => {
-    const {data} = await runFbFunction({
-      type: "SET",
-      key,
-      value
-    });
-    console.log(data);
-    return data;
+    return await fetch(`http://localhost:5000/lildb-8ff35/us-central1/db/${endpoint}`, {
+      method: "POST",
+      body: JSON.stringify({
+        key,
+        value
+      })
+    }).then((response) => response.json());
   },
   update: async (key, value) => {
-    const {data} = await fbFunctions({
+    return await fbFunctions({
       type: "UPDATE",
       key,
       value
     });
-    return data;
   }
 };
