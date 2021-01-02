2b722a5dae21dc15f9f4f68907fd077eeab7218e
Mateo Morris <accounts@mateomorris.com>
2021-01-03 00:37:45 +0200

    clean up

--- library/lildb.js
+++ library/lildb.js
@@ -1,23 +1,4 @@
 let endpoint
-async function runFbFunction(args) {
-  if (endpoint) {
-    // return await fbFunctions({ ...args, endpoint })
-    // const foo = await fetch('http://localhost:5000/lildb-8ff35/us-central1/db', args)
-    return fetch(`http://localhost:5000/lildb-8ff35/us-central1/db/foo?endpoint=${endpoint}&key=${args.key}`, {
-      method: 'GET',
-      // body: JSON.stringify(args)
-    })
-      .then(response => response.json())
-  } else {
-    console.error('lildb needs an endpoint')
-    return {
-      data: null
-    }
-  }
-}
-
-// an api to modify, get, and set data
-
 export default {
   connect: (end) => {
     endpoint = end
